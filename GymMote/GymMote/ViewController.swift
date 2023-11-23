0a59824548ee39e017d1495aa4a42c477d30745f
Sabina Grinenko <kaiser2169@gmail.com>
2023-11-23 16:22:00 +0400

    Init commit

---
+++ GymMote/GymMote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GymMote
+//
+//  Created by Sabina Grinenko on 23.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
